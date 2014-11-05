0f0cefea21fe6a42bf9e96eab7cf4b4e418dec27
BenSower <sauer.benjamin@gmail.com>
2014-11-05 19:31:58 +0100

    refactored query functions

--- backend/mysql.js
+++ backend/mysql.js
@@ -15,36 +15,41 @@ mysqlTunnel.connect(function (err) {
 	}
 	console.log("tunnel connected");
     //call query function
-	mysqlQuery(mysqlTunnel);    
-    
-
+	mysqlConnect(mysqlTunnel);    
 });
 
 
-function mysqlQuery(tunnel){
+function mysqlConnect(tunnel){
     
 	console.log("Mysql Query starting");
 
 	var connection = mysql.createConnection(config.database);
 
 	connection.connect(function(err) {
-	  if (err) {
-	    console.error('error connecting: ' + err.stack);
-	    return;
-	  }
-	  console.log('connected as id ' + connection.threadId);
-	  connection.query("SELECT count(*) AS solution FROM MediaQ_V2.VIDEO_INFO ", function(err, rows, fields) {
-		  if (err) throw err;
-		  console.log(rows);
-		  console.log('The solution is: ', rows[0].solution);
-	  });
-	  
-	  connection.end();
-	  console.log("close tunnel");
-	  tunnel.close();
+		if (err) {
+		  console.error('error connecting: ' + err.stack);
+		  return;
+		}
+		
+		console.log('connected to DB ' + config.remoteHost);
+		
+		var query = "SELECT count(*) AS solution FROM MediaQ_V2.VIDEO_INFO;";
+		var rows = mysqlQuery(connection, 
+								query, 
+								function(rows) {
+									console.log("Return value is: \n" + rows[0].solution);
+								});
+		
+		connection.end();
+		console.log("closing tunnel");
+		tunnel.close();
 	});	
-	
-	
-	
+}
 
+function mysqlQuery(connection, query, callback){
+	connection.query(query, function(err, rows, fields) {
+		  if (err) throw err;
+	  	  callback(rows);
+		  return rows;
+	});
 }
