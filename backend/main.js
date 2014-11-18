02e546ea3f1b5261fe9a5deb38e13bbaecc59a55
Ben Sower <sauer.benjamin@gmail.com>
2014-11-18 01:53:42 +0100

    Simplification of the test

--- backend/main.js
+++ backend/main.js
@@ -3,12 +3,11 @@ var MysqlTunnel = require ('./mysqlTunnelModule');
 //creates a tunneled mysql connection
 var tunnel = new MysqlTunnel();
 
-//waits and fires a test query
-test();
-function test(){
-    var query = "SELECT count(*) AS solution FROM MediaQ_V2.VIDEO_INFO;";
-    var rows = tunnel.query(query,function(rows) {
-            console.log("Current number of uploaded videos is: " + rows[0].solution);
-    },10);
-}
+//fires a test query
+
+var query = "SELECT count(*) AS solution FROM MediaQ_V2.VIDEO_INFO;";
+var rows = tunnel.query(query,function(rows) {
+        console.log("Current number of uploaded videos is: " + rows[0].solution);
+},10);
+
 
