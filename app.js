49f4a1ea35b8a65bcca77cf32b0b9e8758c63bf1
BenSower <sauer.benjamin@gmail.com>
2015-01-05 13:13:03 +0100

    disabled mysql and added basic html structure

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
+var desktop = require('./routes/desktop');
+var mobile = require('./routes/mobile');
+
 
 var app = express();
 
@@ -22,8 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/desktop', desktop);
+app.use('/mobile', mobile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-        remoteHost: '<mediaq server>', // e.g. mediaq.dbs.ifi.lmu.de
+        remoteHost: 'mediaq.dbs.ifi.lmu.de', // mediaq server host
         remotePort: 3306, // mediaq-mysql server port
         localPort: 31337, // a available local port
         verbose: true, 	// dump information to stdout
         sshConfig: {             
-        	host: '<tunnel host>', //e.g. remote.cip.ifi.lmu.de
+        	host: 'remote.cip.ifi.lmu.de', 
             port: 22,
-            username: '<cip username>',
-            password: '<cip pw>', // option see ssh2 config,
+            username: 'sauerb',
+            password: '***REMOVED***', // option see ssh2 config,
         },
         database: {
            host: "127.0.0.1",
            port: 31337,
-           user: "<mediaq db-user>", //e.g. student
-           password: "<mediaq db-pw>",
-           database: "<mediaq DB>" //e.g. MediaQ_V2
+           user: "student",
+           password: "***REMOVED***",
+           database: "MediaQ_V2"
        }
-}; 
+};
