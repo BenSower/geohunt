a089a71296acbb9df40d4c8aeb5fa138305e3409
BenSower <sauer.benjamin@gmail.com>
2015-02-26 17:54:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     database: {
         host: 'mediaq.dbs.ifi.lmu.de',
         port: 3306,
-        user: 'student',
-        password: '***REMOVED***',
+        user: '',
+        password: '',
         database: 'MediaQ_V2'
     },
     sessionSecret: 'flkdfkföldrfkö',
